918ded787d2a27656667045ac2b059340ac45077
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-18 20:50:14 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -1,2 +1,10 @@
 THEME_COLOR = "#375362"
+from tkinter import *
+class QuizInterface:
 
+    def __int__(self):
+        self.window = Tk()
+        self.window.title("Quizzler")
+        self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+
+        self.window.mainloop()
