2299eae2452fdd279bae2794914ef4a0e5fe1cd9
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-19 20:16:45 +0530

    ui setup

--- ui.py
+++ ui.py
@@ -1,10 +1,16 @@
 THEME_COLOR = "#375362"
 from tkinter import *
+
 class QuizInterface:
 
     def __int__(self):
         self.window = Tk()
         self.window.title("Quizzler")
         self.window.config(padx=20,pady=20,bg=THEME_COLOR)
+        self.score_label = Label(text="Score: 0", fg="white",bg =THEME_COLOR)
+        self.score_label.grid(row=0,column=1)
+
+        self.canvas = Canvas(width=300,height=250,bg="white")
+        self.question_text = self.canvas.create_text(text="Some Question Text",fill=THEME_COLOR)
 
         self.window.mainloop()
