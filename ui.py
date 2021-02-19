71cb65a3bf61364f5d3477d2ac298e5f39979e4d
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-19 20:22:57 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -12,5 +12,5 @@ class QuizInterface:
 
         self.canvas = Canvas(width=300,height=250,bg="white")
         self.question_text = self.canvas.create_text(text="Some Question Text",fill=THEME_COLOR)
-
+        self.canvas.grid(row=1,column=0, columnspan=2)
         self.window.mainloop()
