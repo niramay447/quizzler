96db5e572c0fb617950d295bc07164af5e7c1ce0
niramay447 <54571224+niramay447@users.noreply.github.com>
2021-02-19 20:41:17 +0530

    Add files via upload

--- ui.py
+++ ui.py
@@ -15,13 +15,17 @@ class QuizInterface:
             150,
             125,
             text="Some Question Text",
-            fill=THEME_COLOR
-            font = ("Arial",20,"italic")
+            fill=THEME_COLOR,
+            font = ("Arial",20,"italic"),
         )
         self.canvas.grid(row=1,column=0, columnspan=2, pady=50)
 
         true_image = PhotoImage(file="true.png")
         self.true_button = Button(image=true_image,highlightthickness=0)
         self.true_button.grid(row=2,column =0)
+
+        false_image = PhotoImage(file="false.png")
+        self.false_button = Button(image=false_image, highlightthickness=0)
+        self.false_button.grid(row=2,column=1)
 
         self.window.mainloop()
